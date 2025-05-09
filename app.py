72250d7b0531a29e67c33fd19c524705943a31c1
jiwon07380 <102030078+jiwon07380@users.noreply.github.com>
2025-05-10 01:32:59 +0900

    Update app.py
    앱 시작 시 보드 초기화

--- app.py
+++ app.py
@@ -58,6 +58,8 @@ def reset():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == "__main__":
+    reset_board()  # ✅ 앱 시작 시 보드 초기화
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
